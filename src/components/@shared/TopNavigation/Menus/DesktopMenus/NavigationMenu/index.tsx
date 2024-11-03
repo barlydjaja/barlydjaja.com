a6463439d03d1e858178afa900ac9038d518c79b
Barly <barlydjaja@gmail.com>
2024-11-03 16:15:08 +0700

    feat add contact me button

--- src/components/@shared/TopNavigation/Menus/DesktopMenus/NavigationMenu/index.tsx
+++ src/components/@shared/TopNavigation/Menus/DesktopMenus/NavigationMenu/index.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 const NavigationMenu = ({menuList}: MenusProps) => {
   const pathname = usePathname()
   return (
-    <div className="flex items-center justify-between py-2 px-12 border rounded-full bg-[#222222] shadow-inner backdrop-blur-2xl background-white gap-10">
+    <div className="flex items-center justify-between py-3 px-12 border rounded-full bg-[#222222] shadow-inner backdrop-blur-2xl background-white gap-10">
       {menuList.map(menu => {
         const isActivePath = pathname === menu.link
 
--- src/components/@shared/TopNavigation/Menus/DesktopMenus/index.tsx
+++ src/components/@shared/TopNavigation/Menus/DesktopMenus/index.tsx
@@ -1,14 +1,15 @@
 import { MenusProps } from '@/components/@shared/TopNavigation/Menus';
 import NavigationMenu from '@/components/@shared/TopNavigation/Menus/DesktopMenus/NavigationMenu';
+import Link from 'next/link';
 
 const DesktopMenus = ({menuList}: MenusProps) => {
   console.log(menuList)
   return (
     <>
       <NavigationMenu menuList={menuList} />
-      <div>
-        desktop menu
-      </div>
+      <Link href="https://www.linkedin.com/in/barly-djaja/" target="_blank" className="button-accent flex items-center justify-center rounded-full py-3 px-6 mb-2.5 text-base font-medium">
+        Contact Me
+      </Link>
     </>
   );
 };
